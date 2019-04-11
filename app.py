60733c32db325059bc2f189a0dc8a523aac23a5a
zebulon75018 <zebulon75018@yahoo.fr>
2019-04-11 22:25:59 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import sqlite3
+import json
+import glob
+from flask import Flask
+from flask import render_template
+import csv
+
+server = Flask(__name__)
+
+@server.route('/edit/<file>')
+def edit(file):
+    result = []
+    with open(file, 'rb') as csvfile:
+        data = csv.reader(csvfile, delimiter=',')
+        for r in data:
+            result.append(r)
+    return render_template('edit.html',file=file,data=result)
+    
+@server.route('/')
+def hello():
+    dirs = glob.glob("*.csv")
+    return render_template('index.html',dirs=dirs)
+    #return "Hello World!"
+
+       
+if __name__ == '__main__':
+    server.run(debug=True)
