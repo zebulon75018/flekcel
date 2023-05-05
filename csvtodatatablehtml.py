f302653b84364a6af11fff074afe4432056c9e23
cv <zebulon75018@yanhoo.fr>
2023-05-06 00:09:26 +0200

    add generation from csv to datatable html output according to csv

---
+++ csvtodatatablehtml.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from jinja2 import Environment, FileSystemLoader
+import sys
+
+environment = Environment(loader=FileSystemLoader("templates/"))
+template = environment.get_template("datatable.html")
+
+df = pd.read_csv(sys.argv[1],delimiter=";",nrows=1)  
+cols = list(df)
+
+
+print(template.render(cols=cols))
+
